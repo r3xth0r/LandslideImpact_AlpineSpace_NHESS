2446ca5be939dbffe26b6137f09ad47dd820c29c
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-10-21 17:25:08 +0200

    REF/DOC: reorder package imports, refine docstrings

--- StegerEtAl_NHESS.R
+++ StegerEtAl_NHESS.R
@@ -31,16 +31,15 @@ gc()
 
 # Load packages
 library("mgcv") # fitting GAMs (bam/gam)
+library("dplyr") # data manipulation
 library("sf") # spatial vector data (simple features)
-library("pROC") # ROC / AUC calculations
-library("gratia") # diagnostic & plotting helpers for GAMs (draw(), smooths(), etc.)
-library("sperrorest") # spatial cross-validation helper (partition_factor_cv)
 library("ggplot2") # plotting
+library("patchwork") # compose ggplots
 library("tmap") # thematic mapping for sf objects
-library("dplyr") # data manipulation (select, mutate, %>%)
-library("patchwork") # combine ggplot objects
-library("vip") # variable importance (vi_permute)
-
+library("sperrorest") # spatial error estimation
+library("pROC") # ROC / AUC calculations
+library("vip") # variable importance computation
+library("gratia") # diagnostic & plotting helpers for GAMs
 
 ## -------------------------------------------------------------------------- ##
 ## Config ----
